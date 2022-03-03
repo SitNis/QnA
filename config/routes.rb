eb5f98578dbc058dc8f69ceff1baa01933f00e46
SitNis <rus.lol2011@yandex.ru>
2022-03-03 12:34:47 +0300

    routes upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'questions#index'
 
-  resources :questions, shallow: true do
-    resources :answers, shallow: true, except: %i[index show]
+  resources :questions do
+    resources :answers, shallow: true, except: %i[show index]
   end
 end
