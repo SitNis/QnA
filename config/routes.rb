c08da5431d13ff912f6b36fac67d1d5b087338ec
SitNis <rus.lol2011@yandex.ru>
2022-03-13 18:49:21 +0300

    delete links with ajax, refactoring for files

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'questions#index'
 
   resources :files, only: %i[destroy]
+  resources :links, only: %i[destroy]
 
   resources :questions do
     resources :answers, shallow: true, except: %i[show index] do
