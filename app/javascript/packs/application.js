f7b38c1597c5e1387967c557078bca825bbe991e
SitNis <rus.lol2011@yandex.ru>
2022-03-13 16:31:49 +0300

    use gem cocoon to add several links

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'packs/utilities/answers'
 import 'packs/utilities/question'
+require("jquery")
+require("@nathanvda/cocoon")
 
 Rails.start()
 Turbolinks.start()
