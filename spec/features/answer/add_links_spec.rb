09f160541081b93c021f125f9ac143fb75a58fba
SitNis <rus.lol2011@yandex.ru>
2022-03-14 11:27:40 +0300

    add links when edit, test for edit

--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -8,6 +8,8 @@ feature 'User can add links to answer', %q{
 
   given(:user) { create(:user) }
   given(:question) { create(:question) }
+  given!(:answer) { create(:answer, user: user, question: question)}
+  given(:google_url) { 'http://google.com'}
   given(:gist_url) { 'https://gist.github.com/SitNis/7bb571bc6ebe886a0397f2c2fc957f07' }
 
   scenario 'User adds link when answer question', js: true do
@@ -15,13 +17,30 @@ feature 'User can add links to answer', %q{
     visit question_path(question)
 
     fill_in 'Body', with: 'text text text'
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+    fill_in 'Link name', with: 'Google'
+    fill_in 'Url', with: google_url
 
     click_on 'Answer'
 
     within '.answers' do
-      expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'Google', href: google_url
+    end
+  end
+
+  scenario 'User adds link when edit his answer', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    within '.answers' do
+      click_on 'Edit'
+      click_on 'add link'
+
+      fill_in 'Link name', with: 'Google'
+      fill_in 'Url', with: google_url
+
+      click_on 'Save'
+
+      expect(page).to have_link 'Google', href: google_url
     end
   end
 end
--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -9,6 +9,7 @@ feature 'User can add links to question', %q{
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/SitNis/7bb571bc6ebe886a0397f2c2fc957f07' }
   given(:google_url) { 'http://google.com'}
+  given(:question) { create(:question, user: user)}
 
   scenario 'User adds link when asks question' do
     sign_in(user)
@@ -39,5 +40,22 @@ feature 'User can add links to question', %q{
     within '.question' do
       expect(page).to have_content 'test test'
     end
+  end
+
+  scenario 'user adds link when edit his question', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    within '.question' do
+      click_on 'Edit'
+      click_on 'add link', match: :first
+
+      fill_in 'Link name', with: 'Google'
+      fill_in 'Url', with: google_url
+    end
+
+    click_on 'Save'
+
+    expect(page).to have_link 'Google', href: google_url
   end
 end
