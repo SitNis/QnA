782726f191486eef884b56ff36559c964f0dbcdd
SitNis <rus.lol2011@yandex.ru>
2022-04-14 15:37:18 +0300

    comments realization

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   include Voted
+  include Commented
 
   before_action :load_question, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[index show]
--- app/javascript/channels/answers_channel.js
+++ app/javascript/channels/answers_channel.js
@@ -1,15 +1,13 @@
 import consumer from "./consumer"
 
+consumer.subscriptions.create({ channel: "AnswersChannel", question_id: gon.question_id }, {
+  connected() {
+    this.perform('follow')
+  },
 
-  consumer.subscriptions.create({ channel: "AnswersChannel", question_id: gon.question_id }, {
-    connected() {
-      this.perform('follow')
-    },
-
-    received(data) {
-      console.log(data.author_id)
-      if(gon.user_id == data.author_id) return undefined
-      console.log(data)
-      $('.answers').append(data.page)
-    }
-  })
+  received(data) {
+    if(gon.user_id == data.author_id) return undefined
+    console.log(data)
+    $('.answers').append(data.page)
+  }
+})
