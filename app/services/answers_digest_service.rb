4149b32b1b39dfeeb04e7a585e640e0fd6f2ded8
SitNis <rus.lol2011@yandex.ru>
2022-04-20 20:18:55 +0300

    send email about new answer in question

--- app/services/answers_digest_service.rb
+++
@@ -1,7 +0,0 @@
-class AnswersDigestService
-  def send_notification_to_subscribers
-    User.find_each(batch_size: 500) do |user|
-      AnswersDigestMailer.digest(user).deliver_later
-    end
-  end
-end
